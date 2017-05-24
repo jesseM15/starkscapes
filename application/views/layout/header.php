6898aa7c8da17a06b5522d2b4fe20cffb709c8d6
jesse merritt <jessemdev@gmail.com>
2017-05-23 20:46:19 -0400

    Switched to minimized js and css.  Added google analytics code.

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="<?= base_url() ?>assets/js/main.js"></script>
+	<script src="<?= base_url() ?>assets/js/minified/main.min.js"></script>
 	<?php if ($this->uri->segment(1) == 'gallery') : ?>
 	<script  src="<?= base_url() ?>assets/js/lightbox.js"></script>
 	<link href="<?= base_url() ?>assets/css/lightbox.css" rel="stylesheet" type="text/css">
@@ -26,16 +26,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
 	<!-- Include stylesheet -->
-	<link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/minified/style.min.css">
 
-	<!-- <script>
+	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-99294086-1', 'auto');
 		ga('send', 'pageview');
-	</script> -->
+	</script>
 
 </head>
 <body style="background:url(<?= base_url() . (!empty($background['path']) ? $background['path'] : '') ?>) no-repeat center center fixed;-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
